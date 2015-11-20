656e26d737812879fce72ac0a133a6aaf7b13ee4
= <841098845@qq.com>
2015-11-20 13:42:56 +0800

    Fix an Excel Import Bug
    add option which decide whether a record will be check repeated when in
    a insert process to improve insert speed. Do set RepeatCheck to false
    when u make sure it will not repeat!

--- app/Http/Controllers/ExcelImportController.php
+++ app/Http/Controllers/ExcelImportController.php
@@ -10,6 +10,8 @@ namespace App\Http\Controllers;
 use Excel;
 use App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
+
+set_time_limit(0);
 class ExcelImportController extends Controller {
     /*
      * 根据前端导入的Excel文件向数据库中插入原始数据
@@ -36,15 +38,23 @@ class ExcelImportController extends Controller {
         //DB::insert('insert into unit (name,channel_number,receiver_id,norminal_power,power_factor,create_user) values (?, ?, ?, ?, ?, ?)', ['所有',1,'100313',0,1,1]);
     }*/
 
+    protected $channelRecordRepeatCheck = true;
+    protected $orderRecordRepeatCheck = true;
 
     //导入电量数据—此处同时兼任导入温度数据
     public function importPowerRecord()
     {
         $directory = 'storage\app\power\power.csv';
+
         Excel::load($directory, function($reader) {
             // Loop through all sheets
+            $this->channelRecordRepeatCheck = false;
             $reader->each(function($row) {
-                $this->powerRecordInsert($row,1);
+                $create_user = 1;
+                if(empty($channels)) {//此处查询指定create_user的channel号等信息
+                    $channels = DB::table('channel')->select('id', 'channel_number', 'receiver_id')->where('create_user', '=', $create_user)->get();
+                }
+                $this->powerRecordInsert($row,$channels);
                 //暂时所有record都插入到create_user = 1的user名下，日后量化再修改此处.
             });
         });
@@ -54,6 +64,8 @@ class ExcelImportController extends Controller {
     {
         $directory = 'storage\app\order\order.xlsx';
         Excel::load($directory, function($reader) {
+            //set the timestamp format
+            $reader->setDateColumns(array('sendprint_date'));
             // Loop through all sheets
             $reader->each(function($sheet) {
                 // Loop through all rows
@@ -75,7 +87,6 @@ class ExcelImportController extends Controller {
             //查找merchandise_class中的种类的id。
             $merchandise_class = DB::table('merchandise_class')->select('id')->where('name','=',$row['cls_name'])->first();
             $merchandise_class_id = $merchandise_class->id;
-            dump($merchandise_class);
             //添加此商品到merchandise表中并获取商品id
             $merchandise_id = DB::table('merchandise')->insertGetId(
                 array('name'=>$row['item_name'],'merchandise_class'=>$merchandise_class_id)
@@ -84,9 +95,9 @@ class ExcelImportController extends Controller {
         else
         {
             $merchandise_id = $merchandise->id;
-            //dump($merchandise_id);
             //检测订单是否已存在，防止重复插入
-            if($this->orderExist($merchandise_id, $row['sendprint_date']->toDateTimeString()))
+            //如果要求不查重,则直接跳过查重函数。
+            if($this->orderRecordRepeatCheck && $this->orderExist($merchandise_id, $row['sendprint_date']->toDateTimeString()))
                 return ;
         }
         //根据商品id插入order
@@ -103,16 +114,15 @@ class ExcelImportController extends Controller {
     }
 
     //插入一条Power_record记录
-    public function powerRecordInsert($row,$create_user)
+    public function powerRecordInsert($row,$channels)
     {
         $row = $row->toArray();
-        //此处查询指定create_user的channel号等信息
-        $channels = DB::table('channel')->select('id','channel_number','receiver_id')->where('create_user','=',$create_user)->get();
         //根据每个channel的信息来插入数据——如果channel没有定义，则无法插入数据，之后可以考虑如果channel不存在，则在此处直接新建channel
         foreach($channels as $channel)
         {
             //首先检测记录是否已存在，如果存在，则不再插入。
-            if(!$this->powerRecordExist($row['date'],$channel->id,$row['ch'.$channel->channel_number])) {
+            //如果设置为不查重，则直接进入插入
+            if(!$this->channelRecordRepeatCheck || !$this->powerRecordExist($row['date'],$channel->id, $row['ch'.$channel->channel_number])) {
                 //每次插入完全取决于已经定义的channel的id
                 DB::table('channel_record')->insert(
                     array('channel_id' => $channel->id, 'date' => $row['date'], 'value' => $row['ch' . $channel->channel_number])
